e61db394b88b36652dd87bdd15c10015ff73b6ba
yjcws <1531153813@qq.com>
2020-12-20 22:18:08 +0800

    数据表

--- laravel-study.sql
+++ laravel-study.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 19/12/2020 17:35:51
+ Date: 20/12/2020 22:16:55
 */
 
 SET NAMES utf8mb4;
@@ -57,6 +57,27 @@ INSERT INTO `grades` VALUES (1, '数学 ', 1, NULL, NULL);
 INSERT INTO `grades` VALUES (2, '语文', 2, NULL, NULL);
 
 -- ----------------------------
+-- Table structure for migrations
+-- ----------------------------
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE `migrations`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for password_resets
+-- ----------------------------
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE `password_resets`  (
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp(0) NULL DEFAULT NULL
+) ENGINE = MyISAM CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for students
 -- ----------------------------
 DROP TABLE IF EXISTS `students`;
@@ -75,5 +96,44 @@ CREATE TABLE `students`  (
 -- ----------------------------
 INSERT INTO `students` VALUES (1, 'jsdfd', '1', 18, NULL, NULL);
 INSERT INTO `students` VALUES (2, 'asd', '2', 20, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for user_roles
+-- ----------------------------
+DROP TABLE IF EXISTS `user_roles`;
+CREATE TABLE `user_roles`  (
+  `role_id` int(10) NOT NULL,
+  `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `group_id_array` int(10) NULL DEFAULT NULL,
+  `is_role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `role_status` int(10) NULL DEFAULT NULL,
+  `desc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`role_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `Openid` char(28) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'openid',
+  `username` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名称',
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `role_id` int(11) NULL DEFAULT NULL COMMENT '用户角色id',
+  `vender_type` tinyint(1) NULL DEFAULT NULL COMMENT '第三方类型:1.QQ,2:微信',
+  `status` tinyint(1) NULL DEFAULT NULL COMMENT '用户状态：1. 黑名单',
+  `mobile` char(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号码',
+  `login_ip` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '登录ip',
+  `remember_token` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'token验证',
+  `created_at` datetime(0) NULL DEFAULT NULL COMMENT '注册时间',
+  `updated_at` datetime(0) NULL DEFAULT NULL COMMENT '最后一次修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (1, 'oqCL0s8DkK6CZy_cZFEx0TvfUM_Y', '星空', NULL, 1, 1, 1, NULL, '127.0.0.1', 'cmMNR4zCHradzojq21c05DCS1WE8p2fOKUkzGnkiJ59kx2xjkkNSyfcyGdHw', '2020-07-28 08:23:47', '2020-07-28 08:23:47');
 
 SET FOREIGN_KEY_CHECKS = 1;
